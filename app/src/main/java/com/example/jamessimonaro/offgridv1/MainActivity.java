6932d2603ca56e4fc2dc323eeed1078137461e79
James Simonaro <simonaro.james@gmail.com>
2018-03-08 21:05:06 -0800

    I added another activity that will be our web browser and I started to program the web browser itself.

--- app/src/main/java/com/example/jamessimonaro/offgridv1/MainActivity.java
+++ app/src/main/java/com/example/jamessimonaro/offgridv1/MainActivity.java
@@ -1,16 +1,35 @@
 package com.example.jamessimonaro.offgridv1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button browse_btn_;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Wire button to the java code
+        browse_btn_=(Button)findViewById(R.id.browse_btn);
+
+        //On click listener for the browse button
+        browse_btn_.setOnClickListener(new View.OnClickListener() {@Override public void onClick(View v) {  start_browsing();     }});
     }
+
+        public void start_browsing(){
+        Intent myIntent = new Intent(MainActivity.this, WebBrowser.class);
+        MainActivity.this.startActivity(myIntent);
+        }
+
 }
+
+
 
 
 //HELLO THIS IS A TEST!
